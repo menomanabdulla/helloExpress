60dd06f74628193ee1f15daf5cc7700d51ee8477
menomanabdulla <noman.fcibappy@gmail.com>
2018-06-07 16:56:46 +0600

    .ejs template engine include and test different route

--- server.js
+++ server.js
@@ -4,20 +4,54 @@ const app = express()
 var morgan = require('morgan')
 const path = require('path')
 const PORT = process.env.PORT || 3000
-
 app.use(morgan('dev'))
 
+app.set('view engine','ejs')
+app.set('views','./views')
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'index.html'))
+   res.render('index',{
+       title: 'i am the home page',
+       data: 'Say hello form home page'
+   })
 })
 
+
 app.get('/about', (req, res) => {
+    //console.log(res)
     res.json({
         name: 'Noman Abdulla',
         dept: 'CSE 22intake,'
     })
 })
 
+app.get('/users/:id', (req, res) => {
+    res.send(`User ID is ${req.params.id}`)
+})
+
+app.get('/posts/',(req,res)=>{
+    res.render('allposts',{posts})
+})
+
+app.get('/posts/:id', (req, res) => {
+    let id = req.params.id
+    let post = null
+    for(let i = 0;i<posts.length;i++){
+        if(posts[i].id == id){
+            post = posts[i]
+            break
+        }
+    }
+    if(post){
+        res.render('posts',{post})
+    }else{
+        res.render('posts',{post:false})
+    }
+})
+
+app.get('/api/posts', (req, res) => {
+    res.json(posts)
+})
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'404.html'))
@@ -26,3 +60,23 @@ app.get('*', (req, res) => {
 app.listen(PORT,()=>{
     console.log('Server Successfully listen to port '+PORT)
 })
+
+const posts = [
+    {
+        id: 1,
+        title: 'I am post One',
+        content: 'Hello from post One'
+    },{
+        id: 2,
+        title: 'I am post Two',
+        content: 'Hello from post Two'
+    },{
+        id: 3,
+        title: 'I am post Three',
+        content: 'Hello from post Three'
+    },{
+        id: 4,
+        title: 'I am post Four',
+        content: 'Hello from post Four'
+    }
+]
